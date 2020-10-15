7e886b9948314db11700d0399263297971a03f8b
Parinita Vedantam <vedantampari@gmail.com>
2020-10-15 16:43:45 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <h1>Hello!</h1>
+      {/* Spotify logo */}
+      {/* Login with spotify button */}
     </div>
   );
 }
