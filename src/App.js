613a871a179b9cd931b3810d28224082a162e858
Parinita Vedantam <vedantampari@gmail.com>
2020-10-19 10:34:56 +0530

    four

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from'./Login';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
-    const token = getTokenFromUrl();
-    console.log("token herre", token);
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log("user", user);
+
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+      });
+    }
+
+    console.log("Here is the Token", token);
   }, []);
 
-
+  console.log("user", user);
+ 
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <Player />
+        ) : (  
+          <Login />
+        )
+      }
     </div>
   );
 }
