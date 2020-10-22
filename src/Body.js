b1eed3db79f7d1442a66055d41fd4693d73d56ec
Parinita Vedantam <vedantampari@gmail.com>
2020-10-22 17:46:23 +0530

    Song rows done

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import "./Body.css";
+import "./SongRow.css"
 import { useDataLayerValue } from './DataLayer';
 import Header from "./Header";
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import SongRow from './SongRow';
 
 function Body({ spotify }) {
     const [{discover_weekly}, dispatch] = useDataLayerValue();
@@ -21,6 +26,19 @@ function Body({ spotify }) {
                     <h2>Discover Weekly</h2>
                     <p>{discover_weekly?.description}</p>
                 </div>
+            </div>
+
+            <div className="body__songs">
+                <div className="body__icons">
+                    <PlayCircleFilledIcon className="body__shuffle" />
+                    <FavoriteIcon fontSize="large" />
+                    <MoreHorizIcon />
+                </div>
+
+                {/* Lists of songs */}
+                {discover_weekly?.tracks.items.map((item) => (
+                    <SongRow track={item.track} />
+                ))}
             </div>
         </div>
     );
